78848a44dc3ac35a369a248bcfc5e695e26d10ad
Ntando84 <nbikani84@gmail.com>
2026-07-23 11:57:10 +0200

    Create initial project files

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+    alert("Button clicked!");
+
+
+    // Add your own console.log here during the lecture.
+    console.log("hi");
+});
