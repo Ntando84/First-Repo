455c818d1ff800d2cbb7021a681cfac86e7524a0
Ntando84 <nbikani84@gmail.com>
2026-09-15 11:39:27 +0200

    Initial commit - Retail Price Adjustment & Promotion Control System

---
+++ RETAIL PRICE ADJUSTMENT & PROMOTION CONTROL SYSTEM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RETAIL_PRICE_ADJUSTMENT___PROMOTION_CONTROL_SYSTEM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
