c4721fc83b1de33b2e257362e09981f46d5caf32
Ntando84 <nbikani84@gmail.com>
2026-09-15 17:42:11 +0200

    Paper 3 Mock Exam up to Part 2 - Question 2a

---
+++ RETAIL PRICE ADJUSTMENT & PROMOTION CONTROL SYSTEM/IProductValidation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETAIL_PRICE_ADJUSTMENT___PROMOTION_CONTROL_SYSTEM
+{
+    public interface IProductValidation
+    {
+        bool ValidateProductExists(
+            string productCode, 
+            out Product product, 
+            out string errorMessage);
+    }
+}
